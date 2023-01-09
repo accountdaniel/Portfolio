27ce8e1034cd5d3de3dc76cc6571950c0db1acbc
Daniel Celaya <celaya.alex01@gmail.com>
2023-01-09 17:30:22 -0600

    Header

--- react-portfolio/src/sections/header/Header.jsx
+++ react-portfolio/src/sections/header/Header.jsx
@@ -1,9 +1,41 @@
 import "./header.css";
+import HeaderImage from "../../assets/header.jpg";
+import data from "./data";
 
 const Header = () => {
   return (
     <header id="header">
-      <h2>Header</h2>
+      <div className="container header__container">
+        <div className="header__profile">
+          <img src={HeaderImage} alt="Header Portrait" />
+        </div>
+        <h3>Daniel Celaya</h3>
+        <p>
+          Taking it one day at a time is my motto. Trying to plan to far ahead
+          into the future cause distress and agony on our mental when things
+          don't go as planned.
+        </p>
+        <div className="header__cta">
+          <a href="#contact" className="btn primary">
+            Let's Talk
+          </a>
+          <a href="#portfolio" className="btn light">
+            My Work
+          </a>
+        </div>
+        <div className="header__socials">
+          {data.map((item) => (
+            <a
+              key={item.id}
+              href={item.link}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {item.icon}
+            </a>
+          ))}
+        </div>
+      </div>
     </header>
   );
 };
