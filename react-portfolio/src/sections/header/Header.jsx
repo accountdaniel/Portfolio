4f8c25efa39b1619d28a482e40ba9a371b89fe35
Daniel Celaya <celaya.alex01@gmail.com>
2023-01-09 19:18:24 -0600

    Skills and css

--- react-portfolio/src/sections/header/Header.jsx
+++ react-portfolio/src/sections/header/Header.jsx
@@ -11,7 +11,7 @@ const Header = () => {
         </div>
         <h3>Daniel Celaya</h3>
         <p>
-          Taking it one day at a time is my motto. Trying to plan to far ahead
+          Taking it one day at a time is my motto. Trying to plan too far ahead
           into the future cause distress and agony on our mental when things
           don't go as planned.
         </p>
---
+++ react-portfolio/src/sections/services/data.js
@@ -0,0 +1,26 @@
+import { RiReactjsLine } from "react-icons/ri";
+import { SiAdobexd } from "react-icons/si";
+// import {FaServer} from 'react-icons/fa';
+
+const data = [
+  {
+    id: 1,
+    icon: <SiAdobexd />,
+    title: "UI/UX Design",
+    desc: "Designs are modern and intuitive. I will be using industry-standard rules to ensure users have fun with features",
+  },
+  {
+    id: 2,
+    icon: <RiReactjsLine />,
+    title: "Frontend Development",
+    desc: "Websites looks amazing and will be accessible on mobile, and desktop views",
+  },
+  //   {
+  //   id: 3,
+  //   icon: <FaServer />,
+  //   title: 'Bankend Development',
+  //   desc: 'Learning backend development will become my go to, to ensure security of a client from the beginning.'
+  //   }
+];
+
+export default data;
